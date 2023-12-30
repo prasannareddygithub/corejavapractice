8e58ec136fafd80017f7ff9c7b0d036a609ec501
prasannareddygithub <prasannareddy29300@gmail.com>
2023-12-30 13:20:49 +0530

    second

---
+++ src/corejava/Prasanna1.java
@@ -0,0 +1,23 @@
+package corejava;
+
+public class Prasanna1 {
+	public static float  m1(int a,float b,int c) {
+		return a+b+c;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( m1(2,3,5));
+
+	}
+
+}
+
+
+
+	
+
+	
+		
+		
